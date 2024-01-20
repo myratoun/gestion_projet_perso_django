e309e02be086351f27d9f63909608806277bd48b
myratoun <ny.rakotomanga@gmail.com>
2024-01-20 16:05:45 +0300

    feat: login page

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse(f"<h1>Hello World !</h1>")
+
+
+def login(request):
+    return render(request, 'projects/login.html')
