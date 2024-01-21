1267d81d08180946960f92694adb6e7825b72e57
myratoun <ny.rakotomanga@gmail.com>
2024-01-21 08:33:59 +0300

    fix: indentation

--- gestionprojets/urls.py
+++ gestionprojets/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.urls import path
 from projects.views import home, login
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("",home,name="home"),
-    path("login/", login, name="login-page")
-]
+                  path("admin/", admin.site.urls),
+                  path("", home, name="home"),
+                  path("login/", login, name="login-page")
+              ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
